140ee4c8997239f5897935dbb959826ceaad2c6e
Moshe Srur <moshiko1988@gmail.com>
2018-04-08 01:14:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,9 +2,9 @@ var playlist = {
   britney: 'Hit me baby'
 }
 
-function updatePlaylist(obj, artist, title) {
+function updatePlaylist(playlist, artistName, songTitle) {
 
-  return Object.assign({}, obj, { [artist]: title })
+  return Object.assign({}, playlist, { [artistName]: songTitle })
 }
 
 // Create a function updatePlaylist that accepts three parameters: the playlist (an object), an artist name (a string), and a song title. The body of the function should add the song and artist as a key-value pair to the playlist object. The function should return the whole playlist.
