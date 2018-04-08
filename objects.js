32a39dc90566d14a5097b8d0dbe6f9b7e131f94f
Moshe Srur <moshiko1988@gmail.com>
2018-04-08 01:18:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
