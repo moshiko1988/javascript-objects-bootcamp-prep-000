fcb50eee17d69b51a65c74b2b6f8fddd0c5b45aa
Moshe Srur <moshiko1988@gmail.com>
2018-04-08 01:18:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,5 +6,3 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
-
-// Create a function updatePlaylist that accepts three parameters: the playlist (an object), an artist name (a string), and a song title. The body of the function should add the song and artist as a key-value pair to the playlist object. The function should return the whole playlist.
